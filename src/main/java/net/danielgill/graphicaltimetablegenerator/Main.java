5be597c8b880572aee3ebafebbf6d7807631a418
Daniel-Gill <56364196+Daniel-Gill@users.noreply.github.com>
2020-12-24 18:08:57 +0000

    Added second test file

--- src/main/java/net/danielgill/graphicaltimetablegenerator/Main.java
+++ src/main/java/net/danielgill/graphicaltimetablegenerator/Main.java
@@ -5,7 +5,7 @@ import org.jfree.chart.ui.UIUtils;
 
 public class Main{
     public static void main(String[] args){
-        File tbfile = new File("C:/Users/dan23/Documents/NetBeansProjects/GraphicalTimetableGenerator/src/test/java/test.txt");
+        File tbfile = new File("C:/Users/dan23/Documents/NetBeansProjects/GraphicalTimetableGenerator/src/test/java/test2.txt");
         GraphGenerator demo = new GraphGenerator("Timetable", tbfile);
         demo.pack();
         UIUtils.centerFrameOnScreen(demo);
